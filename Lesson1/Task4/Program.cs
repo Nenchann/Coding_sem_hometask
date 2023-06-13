c99c1579688c2ccb211082566a48c82a8d3eee4c
Nenchan <43211473+Nenchan@users.noreply.github.com>
2023-06-13 16:48:36 +0500

    выполнил 4 задание

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите число для проверки <- ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 2) 
+{
+    System.Console.WriteLine("None");
+}
+else
+{
+    int current = 2;
+    while (current <= num)
+    {
+        System.Console.Write($"{current} ");
+        current += 2;
+    }
+}
