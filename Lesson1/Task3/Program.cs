97d2a60e6b255496a62cbefefd8d49aac0bacfae
Nenchan <43211473+Nenchan@users.noreply.github.com>
2023-06-13 16:35:39 +0500

    выполнил 3 задание

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число для проверки <- ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num%2 == 0) 
+{
+    System.Console.WriteLine($"Число {num} - чётное");
+}
+else
+{
+    System.Console.WriteLine($"Число {num} - нечётное");
+}
