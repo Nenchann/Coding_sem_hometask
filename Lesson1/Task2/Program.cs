525d2aabf93f6596220586b4a091817959c92d32
Nenchan <43211473+Nenchan@users.noreply.github.com>
2023-06-13 16:27:42 +0500

    Не ясны условия второго задания, как мы получаем эти числа? В одну строку? В три строки? Выполнил задание учитывая, что берем все из одной строки

---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+
+string[] i = Console.ReadLine().Split(' ', ',', '.', ':');
+int a = Convert.ToInt32(i[0]);
+int b = Convert.ToInt32(i[1]);
+int c = Convert.ToInt32(i[2]);
+Console.WriteLine(Math.Max(a, Math.Max(b, c)));
